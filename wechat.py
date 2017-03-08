c5efb7215909d0500c25bfd75b206266bc74a8f6
Vector <1531794@tongji.edu.cn>
2017-03-08 14:59:34 +0800

    watching the current working conditions by wechat APP

---
+++ wechat.py
@@ -0,0 +1,24 @@
+
+import itchat
+import time
+from PIL import ImageGrab
+
+@itchat.msg_register(itchat.content.TEXT)
+def text_reply(msg):
+	massage=["send 'result' to get current screen. or send 'detail' to get details"]
+	if msg['Text'] == "screen":
+		im=ImageGrab.grab()
+		localtime=str(time.time())
+		im.save(localtime+".jpg")
+		itchat.send_image(localtime+".jpg",toUserName='filehelper')
+		return localtime
+	elif msg['Text'] == "detail":
+		f=open('info.log','r')
+		detail=f.readlines()
+		print len(detail),detail[len(detail)-1]
+		itchat.send(detail[len(detail)-1], toUserName='filehelper')
+	else:
+		itchat.send(massage[0], toUserName='filehelper')
+
+itchat.auto_login(hotReload=True)
+itchat.run()
